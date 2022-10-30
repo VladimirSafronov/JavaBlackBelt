798c9ea36da035459d431dd2f0cbd66cb329e34a
VladimirSafronov <safron5000@mail.ru>
2022-10-30 14:24:36 +0300

    created parametrized method

---
+++ src/main/java/generics/ParametrizedMethod.java
@@ -0,0 +1,29 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParametrizedMethod {
+
+  public static void main(String[] args) {
+    List<Integer> arr1 = new ArrayList<>();
+    arr1.add(10);
+    arr1.add(20);
+    arr1.add(5);
+    int a = GenericMethod.getSecondElement(arr1);
+    System.out.println(a);
+
+    List<String> arr2 = new ArrayList<>();
+    arr2.add("abc");
+    arr2.add("def");
+    arr2.add("ghi");
+    String s = GenericMethod.getSecondElement(arr2);
+    System.out.println(s);
+  }
+}
+
+class GenericMethod {
+  public static <T> T getSecondElement(List<T> arr) {
+    return arr.get(1);
+  }
+}
