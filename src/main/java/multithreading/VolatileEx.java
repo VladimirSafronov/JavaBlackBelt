3f808dc42a9f80128dbe550188f2f15d5db441a5
VladimirSafronov <safron5000@mail.ru>
2022-11-06 23:33:44 +0300

    practised with volatile

---
+++ src/main/java/multithreading/VolatileEx.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class VolatileEx extends Thread {
+
+  volatile boolean b = true;
+
+  @Override
+  public void run() {
+    long counter = 0;
+    while (b) {
+      counter++;
+    }
+    System.out.println("Loop is finished. counter = " + counter);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    VolatileEx thread = new VolatileEx();
+    thread.start();
+    Thread.sleep(3000);
+    thread.b = false;
+    thread.join();
+    System.out.println("Programme ends");
+  }
+}
