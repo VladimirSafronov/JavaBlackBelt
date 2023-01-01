38026c8f76eb393dd8785d51e0768b31ecd5f4d5
VladimirSafronov <safron5000@mail.ru>
2023-01-01 21:06:45 +0600

    practised with Semaphore

---
+++ src/main/java/multithreading/SemaphoreEx2.java
@@ -0,0 +1,57 @@
+package multithreading;
+
+//parking
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreEx2 {
+
+  private static final boolean[] PARKING_PLACES = new boolean[5];
+  private static final Semaphore SEMAPHORE = new Semaphore(5, true);
+
+  public static void main(String[] args) throws InterruptedException {
+    for (int i = 1; i <= 7; i++) {
+      new Thread(new Car(i)).start();
+      Thread.sleep(400);
+    }
+  }
+
+  public static class Car implements Runnable {
+
+    private final int carNumber;
+
+    public Car(int carNumber) {
+      this.carNumber = carNumber;
+    }
+
+    @Override
+    public void run() {
+      System.out.printf("Автомобиль №%d подъехал к парковке.\n", carNumber);
+      try {
+        SEMAPHORE.acquire();
+        int parkingNumber = -1;
+
+        synchronized (PARKING_PLACES) {
+          for (int i = 0; i < 5; i++) {
+            if (!PARKING_PLACES[i]) {
+              parkingNumber = i;
+              PARKING_PLACES[i] = true;
+              System.out.printf("Автомобиль №%d припарковался на месте %d.\n", carNumber, i);
+              break;
+            }
+          }
+        }
+        Thread.sleep(5000);
+
+        synchronized (PARKING_PLACES) {
+          PARKING_PLACES[parkingNumber] = false;
+        }
+
+        SEMAPHORE.release();
+        System.out.printf("Автомобиль №%d покинул парковку.\n", carNumber);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
